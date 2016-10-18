626fd24853c5cdeecf878d9b80bd1f73bade8d74
linglongxin24 <linglongxin24@163.com>
2016-10-18 16:47:02 +0800

    添加当历史纪录listview中没有数据的时候界面显示暂无数据

--- app/src/main/java/cn/bluemobi/dylan/step/activity/HistoryActivity.java
+++ app/src/main/java/cn/bluemobi/dylan/step/activity/HistoryActivity.java
@@ -3,7 +3,10 @@ package cn.bluemobi.dylan.step.activity;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
+import android.util.TypedValue;
+import android.view.Gravity;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListView;
@@ -51,6 +54,7 @@ public class HistoryActivity extends AppCompatActivity {
     }
 
     private void initData() {
+        setEmptyView(lv);
         if(DbUtils.getLiteOrm()==null){
             DbUtils.createDb(this, "jingzhi");
         }
@@ -65,5 +69,17 @@ public class HistoryActivity extends AppCompatActivity {
                 tv_step.setText(stepData.getStep()+"步");
             }
         });
+    }
+
+    protected <T extends View> T setEmptyView(ListView listView) {
+        TextView emptyView = new TextView(this);
+        emptyView.setLayoutParams(new ViewGroup.LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.MATCH_PARENT));
+        emptyView.setText("暂无数据！");
+        emptyView.setGravity(Gravity.CENTER);
+        emptyView.setTextSize(TypedValue.COMPLEX_UNIT_SP, 16);
+        emptyView.setVisibility(View.GONE);
+        ((ViewGroup) listView.getParent()).addView(emptyView);
+        listView.setEmptyView(emptyView);
+        return (T) emptyView;
     }
 }
