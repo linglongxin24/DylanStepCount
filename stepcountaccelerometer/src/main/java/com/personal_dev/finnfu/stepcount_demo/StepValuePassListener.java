b77dab2e88503de0227bd564bdf0c19d368e77de
DylanAndroid <linglongxin24@163.com>
2017-05-23 10:20:26 +0800

    优化代码，并添加了加速度传感器记步算法，支持左右设备记步

--- stepcountaccelerometer/src/main/java/com/personal_dev/finnfu/stepcount_demo/StepValuePassListener.java
+++
@@ -1,8 +0,0 @@
-package com.personal_dev.finnfu.stepcount_demo;
-
-/**
- * Created by finnfu on 16/9/27.
- */
-public interface StepValuePassListener {
-    public void stepChanged(int steps);
-}
--- stepcountaccelerometer/src/main/java/com/personal_dev/finnfu/stepcount_demo/UpdateUiCallBack.java
+++
@@ -1,8 +0,0 @@
-package com.personal_dev.finnfu.stepcount_demo;
-
-/**
- * Created by finnfu on 16/9/27.
- */
-public interface UpdateUiCallBack {
-    public void updateUi();
-}
